3fc0f2a6818b8f6a0aac750629afc8d9c339262d
Gingejc <gingejc1@gmail.com>
2025-03-31 12:36:31 +0100

    Initialize paul/update-wallet-connectors

---
+++ src/wagmi.ts
@@ -0,0 +1,36 @@
+'use client';
+
+import { useMemo } from 'react';
+import { http, createConfig } from 'wagmi';
+import { base, baseSepolia } from 'wagmi/chains';
+import { coinbaseWallet } from 'wagmi/connectors';
+import { NEXT_PUBLIC_WC_PROJECT_ID } from './config';
+
+export function useWagmiConfig() {
+  const projectId = NEXT_PUBLIC_WC_PROJECT_ID ?? '';
+  if (!projectId) {
+    const providerErrMessage =
+      'To connect to all Wallets you need to provide a NEXT_PUBLIC_WC_PROJECT_ID env variable';
+    throw new Error(providerErrMessage);
+  }
+
+  return useMemo(() => {
+    const wagmiConfig = createConfig({
+      chains: [base, baseSepolia],
+      // turn off injected provider discovery
+      multiInjectedProviderDiscovery: false,
+      connectors: [
+        coinbaseWallet({
+          preference: 'all',
+        }),
+      ],
+      ssr: true,
+      transports: {
+        [base.id]: http(),
+        [baseSepolia.id]: http(),
+      },
+    });
+
+    return wagmiConfig;
+  }, []);
+}
